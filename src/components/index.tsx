27525ce81c4bc4655024d5c10ea8f8067aea5538
Hueu123 <chuhueu1@gmail.com>
2021-12-09 14:53:21 +0700

    index

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,12 +1,14 @@
 export { default as Header } from "./common/Header";
 export { default as Footer } from "./common/Footer";
 export { default as ScrollToTop } from "./common/ScrollToTop";
+//Home1
 export { default as AboutUs1 } from "./home1/AboutUs";
 export { default as ContactUs1 } from "./home1/ContactUs";
 export { default as ContactAnimations1 } from "./home1/ContactUs";
 export { default as CoreServices1 } from "./home1/CoreServices";
 export { default as MainSlider1 } from "./home1/MainSlider";
 export { default as ProfessionalTeams1 } from "./home1/ProfessionalTeams";
+//Home2
 export { default as AboutUs2 } from "./home2/AboutUs";
 export { default as TheBlog } from "./home2/TheBlog";
 export { default as ContactUs2 } from "./home2/ContactUs";
